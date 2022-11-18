36797b35ad7a3cc6c225da807518d0b56835b85e
angelHunther <angelmanuel02401@gmail.com>
2022-11-18 10:42:45 -0600

    intento 3 subiendolo

--- view/proveedor/view/subirImagen.php
+++ view/proveedor/view/subirImagen.php
@@ -10,56 +10,13 @@ require_once '../assets/librerias.php';
 
         <!-- Sidebar -->
         <div id="sidebar-container" class="bg-primary">
-            <div class="logo">
-                <h4 class="text-light font-weight-bold mb-0">Golden</h4>
-            </div>
-            <div class="menu">
-                <a href="view/subirimagen.php" class="d-block text-light p-3 border-0"><i class="icon ion-md-apps lead mr-2"></i>
-                    Subir Imagen</a>
-
-                <a href="#" class="d-block text-light p-3 border-0"><i class="icon ion-md-people lead mr-2"></i>
-                    Mostrar</a>
-
-                <a href="#" class="d-block text-light p-3 border-0"><i class="icon ion-md-stats lead mr-2"></i>
-                    Estadísticas</a>
-                <a href="#" class="d-block text-light p-3 border-0"><i class="icon ion-md-person lead mr-2"></i>
-                    Perfil</a>
-                <a href="#" class="d-block text-light p-3 border-0"> <i class="icon ion-md-settings lead mr-2"></i>
-                    Configuración</a>
-            </div>
+            <?php  require_once 'siderbar.php'; ?>
         </div>
         <div class="w-100">
 
             <!-- Navbar -->
             <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-                <div class="container">
-
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <form class="form-inline position-relative d-inline-block my-2">
-                            <input class="form-control" type="search" placeholder="Buscar" aria-label="Buscar">
-                            <button class="btn position-absolute btn-search" type="submit"><i class="icon ion-md-search"></i></button>
-                        </form>
-                        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                            <li class="nav-item dropdown">
-                                <a class="nav-link text-dark dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <img src="assets/img/user-1.png" class="img-fluid rounded-circle avatar mr-2" alt="https://generated.photos/" />
-                                    proveedor
-                                </a>
-                                <!-- perfin configurar -->
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">Mi perfil</a>
-                                    <a class="dropdown-item" href="#">Suscripciones</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#">Cerrar sesión</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <?php require_once 'navBar.php'; ?>
             </nav>
             <!-- Fin sidebar -->
             <!-- Fin Navbar -->
