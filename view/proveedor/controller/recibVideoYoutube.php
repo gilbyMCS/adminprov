2fa8a1f2a24133d732e157ea5ef5fd9eb2750ef8
angelHunther <angelmanuel02401@gmail.com>
2022-12-04 15:50:31 -0600

    id_video

--- view/proveedor/controller/recibVideoYoutube.php
+++ view/proveedor/controller/recibVideoYoutube.php
@@ -1,8 +1,8 @@
 <?php
 date_default_timezone_set("America/Bogota");
-setlocale(LC_ALL,"es_ES"); 
+setlocale(LC_ALL, "es_ES");
 require_once '../../../assets/conexion.php';
-$conexion=conexion();
+$conexion = conexion();
 
 $fecha              = date("d-m-Y g:i a");
 $nombreVideo   		= $_POST['nombreVideo'];
@@ -16,72 +16,57 @@ $urlVideo2   		= $_POST['urlVideo2'];
 //CODIGO DE VIDEO 1
 $cantidad_url_video 	= strlen($urlVideo);
 if ($cantidad_url_video == '28') {
-	$cortar_url 			= str_replace ('https://youtu.be/','',$urlVideo);
-	$url_final_video 		= 'https://www.youtube.com/embed/' .$cortar_url; 
-
-}elseif ($cantidad_url_video == '41') {
-	$cortar_url = str_replace ('https://m.youtube.com/watch?v=','',$urlVideo);
-	$url_final_video = 'https://www.youtube.com/embed/' .$cortar_url; 
-
-}elseif ($cantidad_url_video == '43') {
-	$cortar_url = str_replace ('https://www.youtube.com/watch?v=','',$urlVideo);
-	$url_final_video = 'https://www.youtube.com/embed/' .$cortar_url; 
-
-}elseif ($cantidad_url_video == '58') {
-	$cortar_url = str_replace ('https://m.youtube.com/watch?v=','',$urlVideo);
-	$url_final_video = 'https://www.youtube.com/embed/' .$cortar_url; 
-
-}elseif ($cantidad_url_video == '60') {
-	$cortar_url = str_replace ('https://www.youtube.com/watch?v=','',$urlVideo);
-	$url_final_video = 'https://www.youtube.com/embed/' .$cortar_url; 
-}else{
-echo "URL INVALIDA";
+	$cortar_url 			= str_replace('https://youtu.be/', '', $urlVideo);
+	$url_final_video 		= 'https://www.youtube.com/embed/' . $cortar_url;
+} elseif ($cantidad_url_video == '41') {
+	$cortar_url = str_replace('https://m.youtube.com/watch?v=', '', $urlVideo);
+	$url_final_video = 'https://www.youtube.com/embed/' . $cortar_url;
+} elseif ($cantidad_url_video == '43') {
+	$cortar_url = str_replace('https://www.youtube.com/watch?v=', '', $urlVideo);
+	$url_final_video = 'https://www.youtube.com/embed/' . $cortar_url;
+} elseif ($cantidad_url_video == '58') {
+	$cortar_url = str_replace('https://m.youtube.com/watch?v=', '', $urlVideo);
+	$url_final_video = 'https://www.youtube.com/embed/' . $cortar_url;
+} elseif ($cantidad_url_video == '60') {
+	$cortar_url = str_replace('https://www.youtube.com/watch?v=', '', $urlVideo);
+	$url_final_video = 'https://www.youtube.com/embed/' . $cortar_url;
+} else {
+	echo "URL INVALIDA";
 }
 
 
 //CODIGO DE VIDEO 2
 $cantidad_url_video2 	= strlen($urlVideo2);
 if ($cantidad_url_video2 == '28') {
-	$cortar_url2 			= str_replace ('https://youtu.be/','',$urlVideo2);
-	$url_final_video2		= 'https://www.youtube.com/embed/' .$cortar_url2; 
-
-}elseif ($cantidad_url_video2 == '41') {
-	$cortar_url2 = str_replace ('https://m.youtube.com/watch?v=','',$urlVideo2);
-	$url_final_video2 = 'https://www.youtube.com/embed/' .$cortar_url2; 
-
-}elseif ($cantidad_url_video2 == '43') {
-	$cortar_url2 = str_replace ('https://www.youtube.com/watch?v=','',$urlVideo2);
-	$url_final_video2 = 'https://www.youtube.com/embed/' .$cortar_url2; 
-
-}elseif ($cantidad_url_video2 == '58') {
-	$cortar_url2 = str_replace ('https://m.youtube.com/watch?v=','',$urlVideo2);
-	$url_final_video2 = 'https://www.youtube.com/embed/' .$cortar_url2; 
-
-}elseif ($cantidad_url_video2 == '60') {
-	$cortar_url2 = str_replace ('https://www.youtube.com/watch?v=','',$urlVideo2);
-	$url_final_video2 = 'https://www.youtube.com/embed/' .$cortar_url2; 
-}else{
-echo "URL INVALIDA";
+	$cortar_url2 			= str_replace('https://youtu.be/', '', $urlVideo2);
+	$url_final_video2		= 'https://www.youtube.com/embed/' . $cortar_url2;
+} elseif ($cantidad_url_video2 == '41') {
+	$cortar_url2 = str_replace('https://m.youtube.com/watch?v=', '', $urlVideo2);
+	$url_final_video2 = 'https://www.youtube.com/embed/' . $cortar_url2;
+} elseif ($cantidad_url_video2 == '43') {
+	$cortar_url2 = str_replace('https://www.youtube.com/watch?v=', '', $urlVideo2);
+	$url_final_video2 = 'https://www.youtube.com/embed/' . $cortar_url2;
+} elseif ($cantidad_url_video2 == '58') {
+	$cortar_url2 = str_replace('https://m.youtube.com/watch?v=', '', $urlVideo2);
+	$url_final_video2 = 'https://www.youtube.com/embed/' . $cortar_url2;
+} elseif ($cantidad_url_video2 == '60') {
+	$cortar_url2 = str_replace('https://www.youtube.com/watch?v=', '', $urlVideo2);
+	$url_final_video2 = 'https://www.youtube.com/embed/' . $cortar_url2;
+} else {
+	echo "URL INVALIDA";
 }
 
 //Creando mi INSERT a BD
-$queryInsert = ("INSERT INTO 
-videos(
-	nombreVideo,
-	urlVideo,
-    nombreVideo2,
-    urlVideo2,
-	fecha
-)
-VALUES (
-	'" .$nombreVideo. "',
-	'" .$url_final_video. "',
-    '" .$nombreVideo2. "',
-	'" .$url_final_video2. "',
-	'" .$fecha. "'
-)");
+$queryInsert = ("INSERT INTO videos(nombreVideo,urlVideo,nombreVideo2,urlVideo2,fecha)VALUES ('" . $nombreVideo . "',
+	'" . $url_final_video . "','" . $nombreVideo2 . "','" . $url_final_video2 . "','" . $fecha . "')");
 $result = mysqli_query($conexion, $queryInsert);
+$ultimoId=mysqli_insert_id($conexion);
+
+if ($result==1) {
+	$query="UPDATE info_negocio SET id_video='$ultimoId'";
+	$res=mysqli_query($conexion, $query);
+}
+
 
 header("Location:../view/subir_video.php");
-
 ?>
